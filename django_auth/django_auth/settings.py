659ab08080f9f8f3e903ed1703a002fa44bcafcf
Kshitiz Pratap <kshitijpratap0809@gmail.com>
2018-05-25 08:25:00 -0400

    added model serializer and view

--- django_auth/django_auth/settings.py
+++ django_auth/django_auth/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+AUTH_USER_MODEL = 'users.User'
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig'
 ]
 
 MIDDLEWARE = [
--- django_auth/users/views.py
+++ django_auth/users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
-# Create your views here.
+class CreateUserAPIView(APIView):
+    # Allow any user (authenticated or not) to access this url 
+    permission_classes = (AllowAny,)
+ 
+    def post(self, request):
+        user = request.data
+        serializer = UserSerializer(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
